f546509bbad7480aef41c84d8a6b1bfacf48645c
prathameshkokadwar04 <prathameshkokadwar123@gmail.com>
2025-11-23 00:19:40 +0530

    basic programs added

--- javabasics.java
+++ javabasics.java
@@ -4,4 +4,25 @@
 //         public static void main(String[] args){
 //             System.out.print("hello");
 //     }
-// }                                      
+// }        
+// import java.util.*;                              
+// public class javabasics{
+//     public static void main(String args[]){
+//         int a = 10;
+//         int b = 5;
+//         System.out.print(a + b);
+//     }
+// // }
+// import java.util.*;
+// public class javabasics{
+//     public static void main(String args[]){
+//         int counter = 1;
+//         System.out.println("Enter the range:");
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         while(counter <= n){
+//             System.out.println(counter+" ");
+//             counter++;
+//         }
+//     }
+// }
