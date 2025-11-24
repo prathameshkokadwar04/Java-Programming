c7902986bf16bcf8c7563e00ec81816e98e2c96a
prathameshkokadwar04 <prathameshkokadwar123@gmail.com>
2025-11-24 23:45:35 +0530

    pattern problem added

--- javabasics.java
+++ javabasics.java
@@ -13,16 +13,14 @@
 //         System.out.print(a + b);
 //     }
 // // }
-// import java.util.*;
-// public class javabasics{
-//     public static void main(String args[]){
-//         int counter = 1;
-//         System.out.println("Enter the range:");
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-//         while(counter <= n){
-//             System.out.println(counter+" ");
-//             counter++;
-//         }
-//     }
-// }
+import java.util.*;
+public class javabasics{
+    public static void main(String args[]){
+        for(int line = 1 ;line < 5 ; line++){
+            for(int star = 1 ; star<=line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
