d6f4bab3849f3f1c2ec7c999d01ebdd2f02abac8
prathameshkokadwar04 <prathameshkokadwar123@gmail.com>
2025-11-25 19:30:22 +0530

    functions file created

---
+++ functions.java
@@ -0,0 +1,8 @@
+public class functions {
+    public static void printHello(){
+        System.out.print("Helloworld");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
--- javabasics.java
+++ javabasics.java
@@ -13,14 +13,37 @@
 //         System.out.print(a + b);
 //     }
 // // }
+
+
+
+
+// ----------------REVERSED STAR PATTERN -----------
+// import java.util.*;
+// public class javabasics{
+//     public static void main(String args[]){
+//         for(int line = 1 ;line < 5 ; line++){
+//             for(int star = 1 ; star<=4-line+1; star++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
 import java.util.*;
 public class javabasics{
-    public static void main(String args[]){
-        for(int line = 1 ;line < 5 ; line++){
-            for(int star = 1 ; star<=4-line+1; star++){
-                System.out.print("*");
-            }
-            System.out.println();
+        public static void printSum(int a,int b) {
+            int sum = a + b;
+            System.out.println("Sum is  " + sum);
         }
+        public static void main(String args[]){
+            System.out.println("Enter two numbers");
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            printSum(a,b);
     }
 }
