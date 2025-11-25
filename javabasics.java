05717ae3659b128ab4b113a6bd0037b2aa976536
prathameshkokadwar04 <prathameshkokadwar123@gmail.com>
2025-11-25 18:16:32 +0530

    reverse star pattern

--- javabasics.java
+++ javabasics.java
@@ -17,7 +17,7 @@ import java.util.*;
 public class javabasics{
     public static void main(String args[]){
         for(int line = 1 ;line < 5 ; line++){
-            for(int star = 1 ; star<=line; star++){
+            for(int star = 1 ; star<=4-line+1; star++){
                 System.out.print("*");
             }
             System.out.println();
