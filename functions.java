e2b6c6e3ac019e9811d53189fe41e569a6c843d4
prathameshkokadwar04 <prathameshkokadwar123@gmail.com>
2025-11-25 19:35:20 +0530

    function with arguement added

--- functions.java
+++ functions.java
@@ -1,8 +1,24 @@
-public class functions {
-    public static void printHello(){
-        System.out.print("Helloworld");
-    }
-    public static void main(String[] args) {
-        printHello();
+// --------------function basic program---------------
+
+// public class functions {
+//     public static void printHello(){
+//         System.out.print("Helloworld");
+//     }
+//     public static void main(String[] args) {
+//         printHello();
+//     }
+//}
+import java.util.*;
+public class functions{
+        public static void printSum(int a,int b) {
+            int sum = a + b;
+            System.out.println("Sum is  " + sum);
+        }
+        public static void main(String args[]){
+            System.out.println("Enter two numbers");
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            printSum(a,b);
     }
 }
